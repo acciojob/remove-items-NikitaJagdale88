fd35dde7d26e495a95adff59ed0462dbbfa6ebab
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-07-10 20:23:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ function removeColor() {
+            var selectElement = document.getElementById("colorSelect");
+            var selectedIndex = selectElement.selectedIndex;
+
+            if (selectedIndex !== -1) {
+                selectElement.remove(selectedIndex);
+            }
+        }
